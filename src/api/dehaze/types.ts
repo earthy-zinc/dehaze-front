79ee41178e7fdaaac0c0819cecea6038449af307
土味锌 <1066365803@qq.com>
2023-11-08 15:23:04 +0800

    修改了图像去雾相关的文件

--- src/api/dehaze/types.ts
+++ src/api/dehaze/types.ts
@@ -1,15 +1,14 @@
-
 export interface ModelInfo {
-	id: number,
-	model_name: string,
-	description: string,
+  value: string;
+  label: string;
+  children?: ModelInfo;
 }
 
 export interface DehazeIndex {
-	psnr: string,
-	ssim: string,
+  psnr: string;
+  ssim: string;
 }
 
 export interface ImageInfo {
-	image_name: string,
+  image_name: string;
 }
--- src/views/demo/dehaze.vue
+++ src/views/demo/dehaze.vue
@@ -2,10 +2,13 @@
 import { calculateIndexApi, dehazeApi, getModelApi } from "@/api/dehaze";
 import { ModelInfo } from "@/api/dehaze/types";
 import SingleUploadPy from "@/components/Upload/SingleUploadPy.vue";
+import { useAppStore } from "@/store/modules/app";
 import { imageBaseURL } from "@/utils/request-py";
+import { ElMessage } from "element-plus";
 
 const dehazeModels = ref<ModelInfo[]>();
 const selectedDehazeModel = ref("");
+const appStore = useAppStore();
 
 onMounted(async () => {
   const { data } = await getModelApi();
@@ -58,8 +61,8 @@ const psnr = ref(0);
 const psnrEvaluate = computed(() => {
   if (psnr.value > 40) return "奇迹";
   else if (psnr.value > 30) return "优秀";
-  else if (psnr.value > 20) return "良好";
-  else if (psnr.value > 10) return "一般";
+  else if (psnr.value > 25) return "良好";
+  else if (psnr.value > 20) return "一般";
   else if (psnr.value > 1) return "不及格";
   else return "-";
 });
@@ -69,16 +72,26 @@ const ssimEvaluate = computed(() => {
   else if (ssim.value > 0.8) return "优秀";
   else if (ssim.value > 0.6) return "良好";
   else if (ssim.value > 0.4) return "一般";
-  else if (ssim.value > 0.1) return "不及格";
+  else if (ssim.value > 0.01) return "不及格";
   else return "-";
 });
 const vi = ref(0);
 const viEvaluate = computed(() => "-");
 const ri = ref(0);
 const riEvaluate = computed(() => "-");
-const comprehensiveReview = computed(
-  () => "该图像去雾效果不错，在PSNR、SSIM指标上的表现都超过了预期，值得庆祝！"
-);
+const comprehensiveReview = computed(() => {
+  if (ssim.value === 0 || psnr.value === 0) return "-";
+  const score = (psnr.value / 40 + ssim.value / 1) / 2;
+  let result;
+  if (score > 0.95) result = "简直是奇迹";
+  else if (score > 0.8) result = "非常优秀";
+  else if (score > 0.6) result = "良好";
+  else if (score > 0.4) result = "一般";
+  else if (score > 0) result = "太差了！不及格";
+  else result = "无法评价";
+  return `该图像去雾效果${result}，在PSNR表现${psnrEvaluate.value}、SSIM指标上的表现${ssimEvaluate.value}！`;
+});
+
 async function calculateDehazeIndex() {
   if (
     hazeImage.height !== clearImage.height ||
@@ -97,16 +110,17 @@ async function calculateDehazeIndex() {
     (progressEvent) => {
       if (progressEvent.total) {
         evaluatePercentage.value = Math.round(
-          (progressEvent.loaded / progressEvent.total) * 100 - 1
+          (progressEvent.loaded / progressEvent.total) * 100
         );
       } else {
         evaluatePercentage.value = 100;
       }
     }
   );
-  psnr.value = parseFloat(data.psnr);
-  ssim.value = parseFloat(data.ssim);
+  psnr.value = Math.floor(parseFloat(data.psnr) * 100) / 100;
+  ssim.value = Math.floor(parseFloat(data.ssim) * 10000) / 10000;
 }
+
 function clearEvaluateResult() {
   clearImage.name = "";
   clearImage.height = 0;
@@ -115,14 +129,17 @@ function clearEvaluateResult() {
   psnr.value = 0;
   ssim.value = 0;
 }
+
 function getHazeImageInfo(width: number, height: number) {
   hazeImage.width = width;
   hazeImage.height = height;
 }
+
 function getClearImageInfo(width: number, height: number) {
   clearImage.width = width;
   clearImage.height = height;
 }
+
 function resetForm() {
   clearEvaluateResult();
   outputImage.name = "";
@@ -133,6 +150,7 @@ function resetForm() {
   hazeImage.height = 0;
   selectedDehazeModel.value = "";
 }
+
 const dialogVisible = ref(false);
 const drawerVisible = ref(false);
 const evaluatePercentage = ref(0);
@@ -140,21 +158,22 @@ const evaluatePercentage = ref(0);
 
 <template>
   <div class="app-container">
-    <h1 style=" margin: 5px 0 10px;text-align: center">多模型图像去雾系统</h1>
+    <h1 style="margin: 5px 0 10px; text-align: center">多模型图像去雾系统</h1>
 
     <div class="operate-panel">
-      <el-select v-model="selectedDehazeModel" placeholder="请选择去雾模型">
-        <el-option
-          v-for="dehazeModel in dehazeModels"
-          :key="dehazeModel.id"
-          :label="dehazeModel.model_name"
-          :value="dehazeModel.model_name"
-        />
-      </el-select>
+      <el-cascader
+        placeholder="请选择去雾模型"
+        v-model="selectedDehazeModel"
+        :options="dehazeModels"
+        filterable
+        :props="{ emitPath: false }"
+        :clearable="true"
+      />
+
       <div class="operate-panel-right">
-        <el-button type="primary" @click="dehazeImage" :loading="isLoading">{{
-          loadingText
-        }}</el-button>
+        <el-button type="primary" @click="dehazeImage" :loading="isLoading"
+          >{{ loadingText }}
+        </el-button>
         <el-button @click="dialogVisible = !dialogVisible">评估效果</el-button>
         <el-button @click="drawerVisible = !drawerVisible">历史记录</el-button>
         <el-button type="warning" @click="resetForm">重置页面</el-button>
@@ -176,7 +195,9 @@ const evaluatePercentage = ref(0);
       >
         <template #error>
           <div class="image-show-placeholder">
-            请上传有雾图像并点击"开始去雾"获取无雾图像
+            <div class="image-show-text">
+              请上传有雾图像<br />并点击"开始去雾"获取无雾图像
+            </div>
           </div>
         </template>
       </el-image>
@@ -184,7 +205,7 @@ const evaluatePercentage = ref(0);
 
     <el-dialog
       v-model="dialogVisible"
-      width="60%"
+      :width="appStore.device === 'mobile' ? '100%' : '70%'"
       title="评估效果"
       top="2vh"
       class="dialog-class"
@@ -204,14 +225,14 @@ const evaluatePercentage = ref(0);
         <div class="dialog-content-right">
           <div class="dialog-content-right-up">
             <el-button type="primary" @click="calculateDehazeIndex"
-              >开始评估</el-button
-            >
+              >开始评估
+            </el-button>
             <el-button type="info" @click="clearEvaluateResult"
-              >清空结果</el-button
-            >
+              >清空结果
+            </el-button>
           </div>
           <div class="dialog-content-right-center">
-            <div style=" display: flex; justify-content: center;width: 100%">
+            <div style="display: flex; justify-content: center; width: 100%">
               <el-progress
                 v-if="evaluatePercentage !== 100"
                 type="dashboard"
@@ -228,33 +249,33 @@ const evaluatePercentage = ref(0);
             </div>
             <div class="text-class">
               <span style="margin-right: 16px">基准图像分辨率</span>
-              {{ clearImage.width + " * " + clearImage.width }}
+              {{ clearImage.width + " * " + clearImage.height }}
             </div>
             <el-descriptions :column="2" size="large">
-              <el-descriptions-item :min-width="50" label="PSNR">{{
-                psnr
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="评价">{{
-                psnrEvaluate
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="SSIM">{{
-                ssim
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="评价">{{
-                ssimEvaluate
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="VI">{{
-                vi
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="评价">{{
-                viEvaluate
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="RI">{{
-                ri
-              }}</el-descriptions-item>
-              <el-descriptions-item :min-width="50" label="评价">{{
-                riEvaluate
-              }}</el-descriptions-item>
+              <el-descriptions-item :span="1" :min-width="50" label="PSNR"
+                >{{ psnr }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="评价"
+                >{{ psnrEvaluate }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="SSIM"
+                >{{ ssim }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="评价"
+                >{{ ssimEvaluate }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="VI"
+                >{{ vi }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="评价"
+                >{{ viEvaluate }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="RI"
+                >{{ ri }}
+              </el-descriptions-item>
+              <el-descriptions-item :min-width="50" label="评价"
+                >{{ riEvaluate }}
+              </el-descriptions-item>
               <el-descriptions-item :width="150" label="综合点评">
                 {{ comprehensiveReview }}
               </el-descriptions-item>
@@ -264,7 +285,12 @@ const evaluatePercentage = ref(0);
       </div>
     </el-dialog>
 
-    <el-drawer v-model="drawerVisible" direction="rtl" title="历史记录">
+    <el-drawer
+      :size="appStore.device === 'mobile' ? '100%' : '650'"
+      v-model="drawerVisible"
+      direction="rtl"
+      title="历史记录"
+    >
       <el-table>
         <el-table-column prop="id" label="序号" />
         <el-table-column prop="hazeModel" label="去雾模型" />
@@ -298,12 +324,18 @@ const evaluatePercentage = ref(0);
 }
 
 .image-show-placeholder {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
   width: 35vmax;
   height: 35vmax;
-  line-height: 35vmax;
-  text-align: center;
   border: 1px dashed var(--el-border-color);
   border-radius: 6px;
+}
+
+.image-show-text {
+  text-align: center;
 }
 
 .dialog-content {
